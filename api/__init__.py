1bc2b63586a95ad4f7f85251678531cc8ace3cb5
huangyuan3h <huangyuan3h@gmail.com>
2021-02-28 23:11:00 +0800

    set up dot env

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,11 @@
 import os
 
 from flask import Flask
+from dotenv import load_dotenv
 
 def create_app(test_config=None):
     # create and configure the app
+    load_dotenv() # loading config
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
@@ -26,6 +28,8 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/hello')
     def hello():
+
+        print(os.getenv('MYSQL_SERVER'))
         return 'Hello, World!'
 
     return app
