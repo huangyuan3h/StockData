a4e4eecf61b5d37936593c2fee84dcf670685c5c
huangyuan3h <huangyuan3h@gmail.com>
2021-03-01 22:47:06 +0800

    update the package of xueqiu

---
+++ xueqiu/list.py
@@ -0,0 +1,38 @@
+import requests
+
+URL = 'https://xueqiu.com/service/v5/stock/screener/quote/list'
+
+PARAMS = {
+    'market': 'CN',
+    'order_by': 'symbol',
+    'type': 'sh_sz',
+    'orderby': 'code',
+    'order': 'asc',
+    'page': '1',
+    'size': '30'
+}
+
+HEADERS = {
+    'Connection': 'keep-alive',
+    'sec-ch-ua': '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
+    'Accept': '*/*',
+    'cache-control': 'no-cache',
+    'X-Requested-With': 'XMLHttpRequest',
+    'sec-ch-ua-mobile': '?0',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Dest': 'empty',
+    'Referer': 'https://xueqiu.com/hq'
+}
+
+
+def get_list(page=1, size=30):
+    PARAMS.update({'page': page, 'size': size})
+    r = requests.get(url=URL, params=PARAMS, headers=HEADERS)
+    return r.json()
+
+
+if __name__ == '__main__':
+    data = get_list(2)
+    print(data)
