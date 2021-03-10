4c1a10326bcde7b051dd65cdab4beb41d4404f68
huangyuan3h <huangyuan3h@gmail.com>
2021-03-10 22:16:44 +0800

    add scheduler

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
 from dotenv import load_dotenv
+from scheduler import start
+
+
+# set configuration values
+class Config(object):
+    SCHEDULER_API_ENABLED = True
+
 
 load_dotenv()  # loading config
 app = Flask(__name__)
+app.config.from_object(Config())
+start(app)
 
-import dao # import the setting
+import dao  # import the setting
+
 
 # a simple page that says hello
 @app.route('/')
---
+++ scheduler/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_apscheduler import APScheduler
+
+
+scheduler = APScheduler()
+
+
+def start(app: Flask):
+    scheduler.init_app(app)
+    scheduler.start()
