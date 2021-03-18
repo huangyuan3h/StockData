fedc2d11c1a4ae8ceb6fda6f0873e901c017130e
huangyuan3h <huangyuan3h@gmail.com>
2021-03-18 23:02:58 +0800

    add kline structure

--- dao/Kline.py
+++ dao/Kline.py
@@ -8,47 +8,19 @@ class Kline(db.Model):
     id = Column(Integer, primary_key=True)
     code = Column(String(16))
     timestamp = Column(TIMESTAMP)
-    volume = Column(Integer) # 成交量
-    open = Column(DECIMAL(8, 2)) # 今开
-    high = Column(DECIMAL(8, 2)) # 最高
-    low = Column(DECIMAL(8, 2)) #最低
+    volume = Column(Integer)  # 成交量
+    open = Column(DECIMAL(8, 2))  # 今开
+    high = Column(DECIMAL(8, 2))  # 最高
+    low = Column(DECIMAL(8, 2))  # 最低
     close = Column(DECIMAL(8, 2))  # 今收
-    chg = Column(DECIMAL(8, 2)) # 涨跌价
-    percent = Column(DECIMAL(8, 2)) # 涨跌价
+    chg = Column(DECIMAL(8, 2))  # 涨跌价
+    percent = Column(DECIMAL(8, 2))  # 涨跌价
+    turnoverrate = Column(DECIMAL(3, 2))  # 换手率
+    amount = Column(DECIMAL(16, 2))  # 成交额
+    pe = Column(DECIMAL(8, 4))  # 市盈率
+    pb = Column(DECIMAL(8, 4))  # 市净率
+    ps = Column(DECIMAL(8, 4))  # 市净率 price-to-sales
+    pcf = Column(DECIMAL(8, 4))  # 市现率
+    market_capital = Column(DECIMAL(16, 2))  # 总市值
 
-#     7: "percent"
-#     8: "turnoverrate"
-#     9: "amount"
-#     10: "volume_post"
-#     11: "amount_post"
-#     12: "pe"
-#     13: "pb"
-#     14: "ps"
-#     15: "pcf"
-#     16: "market_capital"
-#     17: "balance"
-#     18: "hold_volume_cn"
-#     19: "hold_ratio_cn"
-#     20: "net_volume_cn"
-#     21: "hold_volume_hk"
-#     22: "hold_ratio_hk"
-#     23: "net_volume_hk"
-#
-#
-# 7: 0.99
-# 8: 0.25
-# 9: 6352656523
-# 10: null
-# 11: null
-# 12: 57.2142
-# 13: 17.1808
-# 14: 26.8702
-# 15: 59.3062
-# 16: 2550533765208
-# 17: null
-# 18: null
-# 19: null
-# 20: null
-# 21: null
-# 22: null
-# 23: null
+
--- dao/__init__.py
+++ dao/__init__.py
@@ -3,8 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from dao.connection import get_connection_str
 
 app.config['SQLALCHEMY_DATABASE_URI'] = get_connection_str()
+
+
 db = SQLAlchemy(app)
-
-from dao import Stock
-
+from dao import Stock, Kline
 db.create_all()
