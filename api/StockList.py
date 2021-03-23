45def7f34a85b6b5f41d82cdcb8514b856a91665
huangyuan3h <huangyuan3h@gmail.com>
2021-03-23 22:57:05 +0800

    add kline day api

--- api/StockList.py
+++ api/StockList.py
@@ -4,7 +4,9 @@ from flask_restful import Resource
 from api.Response_Code import OK
 from scheduler import scheduler, sync_list
 
-
+'''
+sync all stock data
+'''
 class StockList(Resource):
     def get(self):
         scheduler.add_job(func=sync_list.run, id='sync_list_' + str(datetime.datetime.now())
--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_restful import Api
+
+from api.KLineDay import KLineDay
 from api.StockList import StockList
 from api.Test import Test
 
@@ -7,4 +9,5 @@ from api.Test import Test
 def register_router(app: Flask):
     api = Api(app)
     api.add_resource(StockList, '/sync/StockList')
+    api.add_resource(KLineDay, '/sync/KLineDay')
     api.add_resource(Test, '/test')
