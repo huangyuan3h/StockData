e92ef415cf82b40a2f6f903472ca659ef2cfada9
huangyuan3h <huangyuan3h@gmail.com>
2021-03-24 23:29:41 +0800

    update the table

--- dao/Kline.py
+++ dao/Kline.py
@@ -9,18 +9,18 @@ class Kline(dao.db.Model):
     code = Column(String(16))
     timestamp = Column(TIMESTAMP)
     volume = Column(Integer)  # 成交量
-    open = Column(DECIMAL(8, 2))  # 今开
-    high = Column(DECIMAL(8, 2))  # 最高
-    low = Column(DECIMAL(8, 2))  # 最低
-    close = Column(DECIMAL(8, 2))  # 今收
-    chg = Column(DECIMAL(8, 2))  # 涨跌价
-    percent = Column(DECIMAL(8, 2))  # 涨跌价
-    turnoverrate = Column(DECIMAL(8, 2))  # 换手率
+    open = Column(DECIMAL(16, 2))  # 今开
+    high = Column(DECIMAL(16, 2))  # 最高
+    low = Column(DECIMAL(16, 2))  # 最低
+    close = Column(DECIMAL(16, 2))  # 今收
+    chg = Column(DECIMAL(16, 2))  # 涨跌价
+    percent = Column(DECIMAL(16, 2))  # 涨跌价
+    turnoverrate = Column(DECIMAL(16, 2))  # 换手率
     amount = Column(DECIMAL(16, 2))  # 成交额
-    pe = Column(DECIMAL(8, 4))  # 市盈率
-    pb = Column(DECIMAL(8, 4))  # 市净率
-    ps = Column(DECIMAL(8, 4))  # 市净率 price-to-sales
-    pcf = Column(DECIMAL(8, 4))  # 市现率
+    pe = Column(DECIMAL(16, 4))  # 市盈率
+    pb = Column(DECIMAL(16, 4))  # 市净率
+    ps = Column(DECIMAL(16, 4))  # 市净率 price-to-sales
+    pcf = Column(DECIMAL(16, 4))  # 市现率
     market_capital = Column(DECIMAL(16, 2))  # 总市值
 
     def __repr__(self):
