45c87d480d6a58bdffb4e06059ea393586e352ed
huangyuan3h <huangyuan3h@gmail.com>
2021-03-30 00:51:39 +0800

    update the keywords

--- xueqiu/kline.py
+++ xueqiu/kline.py
@@ -22,11 +22,11 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36',
     'Sec-Fetch-Site': 'same-origin',
     'Sec-Fetch-Mode': 'cors',
-    'Cookie': 'device_id=24700f9f1986800ab4fcc880530dd0ed; xqat=a4b3e3e158cfe9745b677915691ecd794b4bf2f9; xq_r_token=b80d3232bf315f8710d36ad2370bc777b24d5001; xq_id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1aWQiOi0xLCJpc3MiOiJ1YyIsImV4cCI6MTYxNzc2MzQxOCwiY3RtIjoxNjE1Mjk5OTg5MDc4LCJjaWQiOiJkOWQwbjRBWnVwIn0.pclXS_oHSNde7y0388Nr-44-jZyKazKI0fWVGqzSGE5_Qa-fvSaWj4tIc1O1Gbv6WOz5cuSPpIB--xXlRTWyLSt_aalpyj-_vea4b5nQCo4tiRUE38j5-gGddUKp6S7cymBqZ5SJtSE3bF0an32aOOxbtsOPfcF3FZ7GnQvrnQplyLQ1vKws0R5p4HbQtgetzzMQrw_5Qk7iE1FRxuIM5fuEquI2Ntzb23qDDubLbEuwAyzHxDRTKK-skKbpJLgQSL9CVLGCMAvgKKFXlYmUvhpTpfeQu14KnJVN1c_uibByNFDtXGGGeaaKeVK1ye8bHVwGSDn6BxDtEhX07H2uVw; u=851615300020294; Hm_lvt_1db88642e346389874251b5a1eded6e3=1613878569,1614605844,1614613218; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1615300482'
+    'Cookie': 'device_id=24700f9f1986800ab4fcc880530dd0ed; s=cr11nu60al; xq_a_token=a4b3e3e158cfe9745b677915691ecd794b4bf2f9; xqat=a4b3e3e158cfe9745b677915691ecd794b4bf2f9; xq_r_token=b80d3232bf315f8710d36ad2370bc777b24d5001; xq_id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1aWQiOi0xLCJpc3MiOiJ1YyIsImV4cCI6MTYxNzc2MzQxOCwiY3RtIjoxNjE1NzIzMjkyNTU0LCJjaWQiOiJkOWQwbjRBWnVwIn0.lIXUujg17WETI909RwWpD0lXYbHR-vfM-SrVhaefRiOKNJbnfszOYjGJmIgsjl93FWw_l6OjEI6JcTVtVh370bTjNTMxUPxPQ0kDRfdbd_X8T8NZGDDRe6L0e59Hazbohz_dktSen7BWHnytJqlIRkV3594oY8UVc4FS6Z10_a9v0xQ5fa8nsfQSabmJWocG3VB6UiUr_sj2kn_s3A1MDOTEcvXjWEo7CV_olALDi7xwY1_N8IRsntSer556dy-huGF5ueItvVnrLB1eUsUxr6ToUZISECHJ021N7c5foEaO0KAzCXBTlV_1IndNdOMkgXNNRUCVRvjcPzsw0W-GpQ; u=771615723327105; Hm_lvt_1db88642e346389874251b5a1eded6e3=1614605844,1614613218,1615723328,1617033253; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1617034824',
 }
 
 
-def get_data(code='SH600519', begin=get_current_timestamp_millisecond(), search_type='after', period='day', count='1'):
+def get_data(code='SH600519', begin=get_current_timestamp_millisecond(), search_type='before', period='day', count='1'):
     PARAMS.update({'symbol': code, 'begin': begin, 'period': period, 'type': search_type,
                    'count': count})
     r = requests.get(url=URL, params=PARAMS, headers=HEADERS)
