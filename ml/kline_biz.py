e5074c5f8dfd378548b352095f6898255122d531
huangyuan3h <huangyuan3h@gmail.com>
2021-12-30 22:06:25 +0800

    :recycle: update logic for training 1

--- ml/kline_biz.py
+++ ml/kline_biz.py
@@ -1,5 +1,7 @@
 import random
 
+from pandas import DataFrame
+
 import app
 """
 
@@ -12,15 +14,37 @@ from dao.kline_process import get_kline_by_code
 from dao.mapping.kline_mapping import kline_obj_2_dataframe
 from dao.stock_process import get_stock_code_list
 
-
-def kline_process():
-    df = kline_obj_2_dataframe(get_kline_by_code(choose_a_random_stock_code()))
-    print(df.head())
+min_training_size = 63
 
 
 def choose_a_random_stock_code():
     stock_list = get_stock_code_list()
     return random.choice(stock_list)
+
+
+def get_stock_dataframe():
+    return kline_obj_2_dataframe(get_kline_by_code(choose_a_random_stock_code()))
+
+
+def get_valid_data():
+    while True:
+        df = get_stock_dataframe()
+        if len(df.index) > min_training_size:
+            return df
+
+
+def get_63_records_data(df: DataFrame, offset=0):
+    if (offset + min_training_size) > len(df.index):
+        return None
+    else:
+        return df[offset:offset+min_training_size]
+
+
+def kline_process():
+    df = get_valid_data()
+    df2 = get_63_records_data(df)
+    print(df2.head())
+    print(len(df2.index))
 
 
 if __name__ == '__main__':
