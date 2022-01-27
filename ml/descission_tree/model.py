d7bc616c50035c68fce00eec2b2149a3ddb386ca
huangyuan3h <huangyuan3h@gmail.com>
2022-01-27 22:47:57 +0800

    :recycle: update the model

--- ml/descission_tree/model.py
+++ ml/descission_tree/model.py
@@ -1,6 +1,24 @@
+import joblib
 from sklearn.tree import DecisionTreeRegressor
 
-decision_tree = DecisionTreeRegressor()
+
+class DecisionTreeModel(object):
+    loaded = False
+
+    path = 'model_data/decision_tree.pkl'
+
+    def __init__(self):
+        self.decision_tree = DecisionTreeRegressor()
+
+    def load_model(self):
+        self.loaded = True
+        self.decision_tree = joblib.load(self.path)
+
+    def save_model(self):
+        joblib.dump(self.decision_tree, self.path)
+
+    def fit(self, X, y):
+        self.decision_tree.fit(X, y)
 
 
-
+decision_tree = DecisionTreeModel()
--- tasks/training_decision_tree.py
+++ tasks/training_decision_tree.py
@@ -2,16 +2,19 @@ from log import log
 from ml.descission_tree.DescissionDataset import DecisionTreeDataset
 from ml.descission_tree.model import decision_tree
 from task_manager import task_manager
-import joblib
 
 
 @task_manager.celery.task()
 def training_decision_tree(batch_size=10):
+
+    if not decision_tree.loaded:
+        decision_tree.load_model()
+
     for i in range(batch_size):
         ds = DecisionTreeDataset(60, 10, 1000)
         X, y = ds.get_data_set()
         decision_tree.fit(X, y)
 
-    joblib.dump(decision_tree, 'model_data/decision_tree.pkl')
+    decision_tree.save_model()
     log.info("training decision tree %s times", batch_size)
 
