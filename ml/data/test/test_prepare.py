f8575f4f9c53a08c9726256a86136a92e505569c
huangyuan3h <huangyuan3h@gmail.com>
2022-02-02 16:33:44 +0800

    :white_check_mark: update the test for test prepare

--- ml/data/test/test_prepare.py
+++ ml/data/test/test_prepare.py
@@ -1,7 +1,7 @@
 from pandas import DataFrame
 
 from ml.data.prepare import choose_a_random_stock_code, get_stock_data, get_stock_data_greater_then_min_size, \
-    get_stock_data_by_size, get_change_by_mask_size
+    get_stock_data_by_size, get_change_by_mask_size, normalize_stock_data
 
 
 def test_choose_a_random_stock_code(mocker):
@@ -25,19 +25,31 @@ def test_get_stock_data(mocker):
 
 def test_get_stock_data_greater_then_min_size(mocker):
     mock_get_stock_data = mocker.patch('ml.data.prepare.get_stock_data')
-    mock_get_stock_data.return_value = DataFrame([{1, 2, }, {2, 3}])
+    mock_get_stock_data.return_value = DataFrame([[1, 2], [2, 3]])
     result = get_stock_data_greater_then_min_size(1)
     assert result is mock_get_stock_data.return_value
 
 
 def test_get_stock_data_by_size():
-    mock_df = DataFrame([{1, 2, }, {2, 3}])
+    mock_df = DataFrame([[1, 2], [2, 3]])
     result = get_stock_data_by_size(mock_df, 1, 1)
-    assert len(result.compare(DataFrame([{2, 3}]))) == 0
+    assert result.equals(DataFrame([[2, 3]]))
 
 
 def test_get_change_by_mask_size():
-    mock_df = DataFrame([{1, 2}, {2, 3}], columns=["close", "open"])
+    mock_df = DataFrame([[1, 2], [2, 3]], columns=["close", "open"])
     result = get_change_by_mask_size(mock_df, 1)
 
     assert result == -50.0
+
+
+def test_normalize_stock_data():
+    df = DataFrame([[1.0, 2, 'id', 'code', 'time', 10 ** 5, 10 ** 7, 10 ** 9],
+                    [2.0, None, 'id', 'code', 'time', 10 ** 5, 10 ** 7, 10 ** 9],
+                    [4.0, 5, 'id', 'code', 'time', 10 ** 5, 10 ** 7, 10 ** 9]],
+                   columns=["close", "open", "id", "code", "timestamp", "volume", "amount", "market_capital"])
+
+    result = normalize_stock_data(df)
+    assert result.equals(DataFrame([[1.0, 2, 1.0, 1.0, 1.0], [2.0, 3.5, 1.0, 1.0, 1.0], [4.0, 5, 1.0, 1.0, 1.0]],
+                                   columns=["close", "open", "volume", "amount", "market_capital"])
+                         )
