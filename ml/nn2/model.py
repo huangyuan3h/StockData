b33b3dc1db6ca76efbc96aef838a81fba3cfd609
huangyuan3h <huangyuan3h@gmail.com>
2022-02-05 22:15:52 +0800

    :recycle: fix the issue

--- ml/nn2/model.py
+++ ml/nn2/model.py
@@ -3,8 +3,8 @@ from tensorflow.keras import layers, callbacks, Sequential
 
 def get_nn_model(shape_size=840, hidden_layer_size=2048):
     model = Sequential([
-        layers.BatchNormalization(),
-        layers.Dense(hidden_layer_size, activation='relu', input_shape=[shape_size]),
+        layers.BatchNormalization(input_shape=[shape_size]),
+        layers.Dense(hidden_layer_size, activation='relu'),
         layers.Dropout(0.3),
         layers.BatchNormalization(),
         layers.Dense(hidden_layer_size, activation='relu'),
