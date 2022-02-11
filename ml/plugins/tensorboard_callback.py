59af27435655d910c8ac28ce49a984f6984742a8
huangyuan3h <huangyuan3h@gmail.com>
2022-02-11 20:43:07 +0800

    :recycle: fix the issue

--- ml/plugins/tensorboard_callback.py
+++ ml/plugins/tensorboard_callback.py
@@ -2,5 +2,5 @@ from datetime import datetime
 
 from tensorflow import keras
 
-log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+log_dir = "logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
