3edaee68ad4b762311d6f61799d6afc498e74b7b
huangyuan3h <huangyuan3h@gmail.com>
2022-02-12 21:09:57 +0800

    :recycle: update the api

--- api/__init__.py
+++ api/__init__.py
@@ -13,12 +13,12 @@ def register_router(app: Flask):
 
     app.add_url_rule('/tasks', 'tasks', methods=['get'], view_func=get_task)
     # functional endpoint
-    app.add_url_rule('/sync/StockList', 'sync_stock_lists', methods=['get'], view_func=start_sync_stock_list)
+    app.add_url_rule('/sync/stocks', 'sync_stock_lists', methods=['get'], view_func=start_sync_stock_list)
 
-    app.add_url_rule('/sync/KLineDay/<string:code>', 'sync_stock_by_day', methods=['get'],
+    app.add_url_rule('/sync/kline/all', 'sync_all_stock', methods=['get'], view_func=sync_all_stock)
+
+    app.add_url_rule('/sync/kline_by_code/<string:code>', 'sync_stock_by_day', methods=['get'],
                      view_func=start_sync_stock_by_code)
-
-    app.add_url_rule('/sync/KLineAllDay', 'sync_all_stock', methods=['get'], view_func=sync_all_stock)
 
     app.add_url_rule('/stock/<string:code>', 'get_stock_data', methods=['get'], view_func=get_stock_data)
 
