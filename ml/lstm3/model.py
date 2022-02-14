35173ae31d7a8580ae216d00268465aeeef0d0a9
huangyuan3h <huangyuan3h@gmail.com>
2022-02-14 23:27:18 +0800

    :recycle: update the model

--- ml/lstm3/model.py
+++ ml/lstm3/model.py
@@ -1,18 +1,18 @@
-from tensorflow.keras import layers, callbacks, Sequential
+from tensorflow.keras import layers, Sequential
 
 
-def get_lstm3_model(n_steps=60, n_features=14, lstm_layer_size=1024):
+def get_lstm3_model(n_steps=60, n_features=14, lstm_layer_size=2048):
     model = Sequential([
         layers.LSTM(units=lstm_layer_size, return_sequences=True, input_shape=(n_steps, n_features)),
-        layers.LSTM(512, return_sequences=True),
-        layers.LSTM(256, return_sequences=True),
-        layers.LSTM(128, return_sequences=False),
+        layers.LSTM(lstm_layer_size / 2 ** 1, return_sequences=True),
+        layers.LSTM(lstm_layer_size / 2 ** 2, return_sequences=True),
+        layers.LSTM(lstm_layer_size / 2 ** 3, return_sequences=False),
         layers.BatchNormalization(),
         layers.Dropout(0.1),
-        layers.Dense(64, activation='elu'),
+        layers.Dense(lstm_layer_size / 2 ** 4, activation='elu'),
         layers.BatchNormalization(),
         layers.Dropout(0.1),
-        layers.Dense(32, activation='elu'),
+        layers.Dense(lstm_layer_size / 2 ** 5, activation='elu'),
         layers.BatchNormalization(),
         layers.Dropout(0.1),
         layers.Dense(1),
@@ -23,4 +23,3 @@ def get_lstm3_model(n_steps=60, n_features=14, lstm_layer_size=1024):
         metrics=['mae'],
     )
     return model
-
