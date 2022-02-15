500a9f25be6632053c9fbe740e0bf0e27aba5992
huangyuan3h <huangyuan3h@gmail.com>
2022-02-15 22:13:40 +0800

    :recycle: fix the model

--- ml/get_factory.py
+++ ml/get_factory.py
@@ -3,7 +3,11 @@
 def get_factory(model_name='lstm'):
     from ml.lstm.LSTMFactory import LSTMFactory
     from ml.lstm2.LSTMFactory import LSTM2Factory
+    from ml.lstm3.LSTM3Factory import LSTM3Factory
+
     if model_name == 'lstm':
         return LSTMFactory
     if model_name == 'lstm2':
         return LSTM2Factory
+    if model_name == 'lstm3':
+        return LSTM3Factory
