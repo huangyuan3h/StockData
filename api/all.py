952de25551f468b959b4ca36aa3af2171d718426
huangyuan3h <huangyuan3h@gmail.com>
2022-02-16 16:55:53 +0800

    :recycle: update the all function

--- api/all.py
+++ api/all.py
@@ -1,13 +1,13 @@
-from celery import chain, group
+from celery import chain
 
 from api.response_code import OK
 from tasks import sync_kline_day_all, sync_all_fund_flow, generate_report, sync_index_kline_day_all
 
 
 def run_all_daily_task():
-    g = group([sync_index_kline_day_all.s(), sync_kline_day_all.s(),
-               sync_all_fund_flow.s()])
-    c = chain(g,
-              generate_report.s(model_name='lstm', predict_day=3))
+    # g = group([sync_index_kline_day_all.s(), sync_kline_day_all.s(),
+    #            sync_all_fund_flow.s()])
+    c = chain(sync_kline_day_all.s(),
+              generate_report.s(model_name='lstm2', predict_day=3))
     c.delay()
     return OK
