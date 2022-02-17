cc544328f1489d6a39d50bf0221837fcbdfd3333
huangyuan3h <huangyuan3h@gmail.com>
2022-02-17 19:31:00 +0800

    :recycle: update the sync all

--- api/all.py
+++ api/all.py
@@ -1,12 +1,11 @@
 from celery import chain
 
 from api.response_code import OK
-from tasks import sync_kline_day_all, sync_all_fund_flow, generate_report, sync_index_kline_day_all
+from tasks import sync_kline_day_all, sync_all_fund_flow, sync_index_kline_day_all
 
 
 def run_all_daily_task():
     c = chain(sync_index_kline_day_all.s(), sync_kline_day_all.s(),
-              sync_all_fund_flow.s(),
-              generate_report)
+              sync_all_fund_flow.s())
     c.delay()
     return OK
