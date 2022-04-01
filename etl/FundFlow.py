bbc51a63c2be3c8c2703b16c0595fdf5b2c44d4a
huangyuan <huangyuan@hushandeMacBook-Air.local>
2022-04-01 10:57:39 +0800

    :recycle: refactor the logic

--- etl/FundFlow.py
+++ etl/FundFlow.py
@@ -1,22 +1,16 @@
-from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 
-from etl.model import Base
 from etl.model.FundFlow import FundFlow
-from urllib.parse import quote
+from etl.utils import save_data, get_target_engine
 
 if __name__ == '__main__':
-    engine = create_engine("mysql+pymysql://root:root@192.168.31.3/stock", echo=True, future=True)
+    engine = get_target_engine()
     fundingFlows = None
     with Session(engine) as session:
         stmt = select(FundFlow)
         fundingFlows = session.scalars(stmt).all()
-        # session.commit()
 
-    engine1 = create_engine("mysql+pymysql://root:%s@192.168.31.2/stockData" % quote('P@$$uu0rd123'), echo=True,
-                            future=True)
-    Base.metadata.create_all(engine1)
     add_list = []
 
     for s in fundingFlows:
@@ -24,6 +18,4 @@ if __name__ == '__main__':
             FundFlow(code=s.code, timestamp=s.timestamp, main_percent=s.main_percent, huge_percent=s.huge_percent,
                      large_percent=s.large_percent, middle_percent=s.middle_percent, small_percent=s.small_percent))
 
-    with Session(engine1) as session:
-        session.add_all(add_list)
-        session.commit()
+    save_data(add_list)
--- etl/index.py
+++ etl/index.py
@@ -1,27 +1,19 @@
-from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy import select
-
-from etl.model import Base
 from etl.model.Index import Index
-from urllib.parse import quote
+
+from etl.utils import save_data, get_target_engine
 
 if __name__ == '__main__':
-    engine = create_engine("mysql+pymysql://root:root@192.168.31.3/stock", echo=True, future=True)
+    engine = get_target_engine()
     indexes = None
     with Session(engine) as session:
         stmt = select(Index)
         indexes = session.scalars(stmt).all()
-        print(indexes)
-        # session.commit()
 
-    engine1 = create_engine("mysql+pymysql://root:%s@192.168.31.2/stockData"%quote('P@$$uu0rd123'), echo=True, future=True)
-    Base.metadata.create_all(engine1)
     add_list = []
 
     for s in indexes:
         add_list.append(Index(code=s.code, name=s.name))
 
-    with Session(engine1) as session:
-        session.add_all(add_list)
-        session.commit()
+    save_data(add_list)
