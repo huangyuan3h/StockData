34d5f0a2f15dbe09976ed7fab71062345900c38f
huangyuan3h <huangyuan3h@gmail.com>
2022-04-07 19:59:05 +0800

    :recycle: refactor the verify fn

--- ml/data/verify.py
+++ ml/data/verify.py
@@ -1,8 +1,23 @@
-
-
 from sklearn.metrics import mean_absolute_error
 
+from ml.BaseModelFactory import BaseModelFactory
 from ml.data.BaseDataset import BaseDataset
+from ml.data.prepare import choose_random_stock_codes
+
+
+def verify_models(Factory: BaseModelFactory, DataSet: BaseDataset, code_num=500, mask_size=3, paths=[]) -> []:
+    codes = choose_random_stock_codes(code_num)
+    ds = DataSet(mask_size=mask_size)
+    X, y = ds.get_test_data_by_codes(codes)
+
+    result =[]
+    for path in paths:
+        factory = Factory(path=path)
+        model = factory.model
+        predict_y = model.predict(X)
+        res = mean_absolute_error(predict_y, y)
+        result.append(res)
+    return result
 
 
 def verify_by_mean_absolute_error(model):
@@ -10,4 +25,3 @@ def verify_by_mean_absolute_error(model):
     X, y = ds.get_data_set()
     predict = model.predict(X)
     return mean_absolute_error(y, predict)
-
--- ml/lstm4/run.py
+++ ml/lstm4/run.py
@@ -1,42 +1,29 @@
 import app
-from sklearn.metrics import mean_absolute_error
-
-from ml.data.prepare import choose_random_stock_codes
+from ml.data.verify import verify_models
 from ml.lstm4.LSTM4Factory import LSTM4Factory
 from ml.lstm4.VXXDataset import VXXDataset
-
-
-def verify_model(code_num=500, mask_size=3, paths=[]):
-    codes = choose_random_stock_codes(code_num)
-    ds = VXXDataset(mask_size=mask_size)
-    X, y = ds.get_test_data_by_codes(codes)
-
-    for path in paths:
-        factory = LSTM4Factory(path=path)
-        model = factory.model
-        predict_y = model.predict(X)
-        res = mean_absolute_error(predict_y, y)
-        print(res)
-
 
 if __name__ == '__main__':
     """
     lstm4_3 data vs lstm4_3 checkpoint
     """
-    # verify_model(code_num=500, mask_size=3,
-    #              paths=['../../model_data/lstm4_3',
-    #                     '../../model_data/lstm4_3_checkpoint_0328',
-    #                     '../../model_data/lstm4_3_checkpoint_0330'])
+    res = verify_models(Factory=LSTM4Factory, DataSet=VXXDataset, code_num=500, mask_size=3,
+                  paths=['../../model_data/lstm4_3',
+                         '../../model_data/lstm4_3_checkpoint_0330',
+                         '../../model_data/lstm4_3_checkpoint_0402'])
 
     """
     lstm4_1 data vs lstm4_1 checkpoint
     """
-    # verify_model(code_num=500, mask_size=1,
+    #  res = verify_models(Factory=LSTM4Factory, DataSet=VXXDataset,code_num=500, mask_size=1,
     #              paths=['../../model_data/lstm4_1',
     #                     '../../model_data/lstm4_1_checkpoint_0306',
     #                     '../../model_data/lstm4_1_checkpoint_0315'])
 
-    verify_model(code_num=300, mask_size=5,
-                 paths=['../../model_data/lstm4_5',
-                        '../../model_data/lstm4_5_checkpoint_0401',
-                        ])
+    # res = verify_models(Factory=LSTM4Factory, DataSet=VXXDataset, code_num=300, mask_size=5,
+    #              paths=['../../model_data/lstm4_5',
+    #                     '../../model_data/lstm4_5_checkpoint_0405',
+    #                     '../../model_data/lstm4_5_checkpoint_0406',
+    #                     ])
+
+    print(res)
